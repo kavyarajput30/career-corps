602b9c9c2b419b52b1fa4eff82c0705f9978bf0c
kavya rajput <131941640+rajputrajput30@users.noreply.github.com>
2023-09-18 11:54:15 +0530

    Add files via upload

---
+++ src/components/navbar/MainContent.js
@@ -0,0 +1,15 @@
+import './MainContent.css'
+
+function MainContent(){
+    return(
+      <div>
+      <a href ="/">Career programs</a>  
+      <a href ="/">About us</a>
+      <a href ="/">Why us</a>
+      <a href ="/">contact</a>
+      </div>
+    )
+
+}
+
+export default MainContent;
---
+++ src/components/navbar/MainLogo.js
@@ -0,0 +1,9 @@
+import './MainLogo.css'
+
+function MainLogo(){
+   return(
+    <h1>Career Corps</h1>
+   )
+}
+
+export default MainLogo;
